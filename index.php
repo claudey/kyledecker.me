bd59c63402d9b282515c9f548b4bb208799d0ed9
Kyle Decker <kylebdecker@gmail.com>
2014-12-13 23:55:59 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Kyle Decker</title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" /> 
+	<meta name="description" content="The personal homepage for Kyle Decker, designer." />
+	<meta name="keywords" content="web design, development, freelance, greensboro, nc" />
+	<meta name="author" content="Kyle Decker" />
+	<!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> -->
+	<link rel="stylesheet" href="src/css/style.css" type="text/css" />
+	<!-- <link href='http://fonts.googleapis.com/css?family=Vollkorn:400,400italic' rel='stylesheet' type='text/css'> -->
+</head>
+
+<body>
+	<div class="body-wrapper">
+		<header class="header">
+			<div class="wrapper">
+				<!-- <h1 class="site-title" itemprop="name">Kyle Decker</h1> -->
+				<p class="hello">My name is Kyle Decker. I'm a designer, developer, and musician in love with the art of creation.</p>
+			</div>
+		</header>
+		<main class="main">
+			<div class="wrapper">
+				<aside class="info">
+					<div class="info-block">
+						<h4 class="info-header">Contact</h4>
+						<ul class="info-contact">
+							<li><a href="tel:+18288082116" itemprop="telephone">+1&nbsp;(828)&nbsp;808-2116</a></li>
+							<li><a href="mailto:kyle@kyledecker.me" itemprop="email">kyle@kyledecker.me</a></li>
+						</ul>
+					</div>
+					<div class="info-block">
+						<h4 class="info-header">Elsewhere</h4>
+						<ul class="info-elsewhere">
+							<li><a href="http://dribbble.com/kdeckr">Dribbble</a></li>
+							<li><a href="http://facebook.com/kdeckr">Facebook</a></li>
+							<li><a href="http://github.com/kdeckr">Github</a></li>
+							<li><a href="http://instagram.com/kdeckr">Instagram</a></li>
+						</ul>
+					</div>
+					<div class="info-block">
+						<h4 class="info-header">Projects</h4>
+						<ul class="info-projects">
+							<li><a href="http://catnap.co">catnap.co</a></li>
+							<li><a href="http://wintr.mx">wintr.mx</a></li>
+						</ul>
+					</div>
+				</aside>
+				<div class="content">
+					<img src="src/img/selfie.jpg" alt="C'est moi!" class="selfie">
+					<img src="src/img/banner.jpg" alt="C'est moi!" class="banner">
+					<p>I currently design websites for humans at <a href="http://catnap.co">Catnap</a>, a business I co-founded with my boyfriend <a href="http://liamscott.me">Liam Scott</a>. Together we make the web shine.</p>
+					<p>I have been fiddling with web technologies since middle school, and throughout high school and college I have gained experience freelancing and working as part of an agency. I am set to graduate with a degree in Music and Computer Science in about 4 months. <i>(Finally!)</i></p>
+					<p>For my life in bullet points, see my <a href="resume">résumé</a>. If you'd rather chat, send mail to <a href="mailto:kyle@kyledecker.me">kyle@kyledecker.me</a> or come find me in Greensboro and we'll share conversation and coffee. Hope to hear from you soon.</p>
+				</div>
+			</div>
+		</main>
+	</div>
+</body>
+</html> 

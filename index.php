b41423da54a8e894d19c55040e2815dd8d92153b
Kyle Decker <kylebdecker@gmail.com>
2014-12-14 15:22:58 +0100

    Make responsive + update content

--- index.php
+++ index.php
@@ -1,36 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" itemscope itemtype="http://www.schema.org/Person">
 <head>
 	<title>Kyle Decker</title>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" /> 
 	<meta name="description" content="The personal homepage for Kyle Decker, designer." />
-	<meta name="keywords" content="web design, development, freelance, greensboro, nc" />
-	<meta name="author" content="Kyle Decker" />
+	<meta name="author" itemprop="name" content="Kyle Decker" />
 	<!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> -->
 	<link rel="stylesheet" href="src/css/style.css" type="text/css" />
-	<link href='http://fonts.googleapis.com/css?family=Montserrat|Vollkorn:400,400italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Montserrat|Permanent+Marker|Vollkorn:400,400italic' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
 	<div class="body-wrapper">
 		<header class="header">
 			<div class="hello wrapper">
-				<img src="src/img/selfie.jpg" alt="C'est moi!" class="selfie">
-				My name is Kyle Decker. I'm a designer, developer, <span class="amp">&amp;</span> musician in love with the art of creation.
+				<img src="src/img/selfie.jpg" alt="C'est moi!" itemprop="image" class="selfie">
+				My name is Kyle Decker. I'm a designer, developer <span class="amp">&amp;</span> musician in love with the art of <span class="creation">creation</span>.
 			</div>
 		</header>
 		<main class="main">
 			<div class="wrapper">
+				<div class="content">
+					<p>I started fiddling with web technologies in middle school and haven't stopped since. I've worked for clients solo and in small teams, and I will graduate with a degree in Music and Computer Science this May.</p>
+					<p>I currently design websites for humans at <a href="http://catnap.co">Catnap</a>, a business I co-founded with my boyfriend <a href="http://liamscott.me">Liam Scott</a>. Together we make the web shine.</p>
+					<p>For my life in bullet points, see my <a href="resume">résumé</a>. If you'd rather chat, send mail to <a href="mailto:kyle@kyledecker.me">kyle@kyledecker.me</a> or give me a call in Greensboro and we'll share conversation and coffee. Hope to hear from you soon.</p>
+				</div>
 				<aside class="info">
-					<div class="info-block">
+					<div class="info-block info-contact">
 						<h4 class="info-header">Contact</h4>
 						<ul class="info-contact">
 							<li><a href="tel:+18288082116" itemprop="telephone">+1&nbsp;(828)&nbsp;808-2116</a></li>
 							<li><a href="mailto:kyle@kyledecker.me" itemprop="email">kyle@kyledecker.me</a></li>
 						</ul>
 					</div>
-					<div class="info-block">
+					<div class="info-block info-projects">
+						<h4 class="info-header">Projects</h4>
+						<ul class="info-projects">
+							<li><a href="http://catnap.co">catnap.co</a></li>
+							<li><a href="http://wintr.mx">wintr.mx</a></li>
+						</ul>
+					</div>
+					<div class="info-block info-elsewhere">
 						<h4 class="info-header">Elsewhere</h4>
 						<ul class="info-elsewhere">
 							<li><a href="http://dribbble.com/kdeckr">Dribbble</a></li>
@@ -39,19 +50,7 @@
 							<li><a href="http://instagram.com/kdeckr">Instagram</a></li>
 						</ul>
 					</div>
-					<div class="info-block">
-						<h4 class="info-header">Projects</h4>
-						<ul class="info-projects">
-							<li><a href="http://catnap.co">catnap.co</a></li>
-							<li><a href="http://wintr.mx">wintr.mx</a></li>
-						</ul>
-					</div>
 				</aside>
-				<div class="content">
-					<p>I started fiddling with web technologies in middle school and haven't stopped since. I've worked for clients solo and in small teams, and I will graduate with a degree in Music and Computer Science this May.</p>
-					<p>I currently design websites for humans at <a href="http://catnap.co">Catnap</a>, a business I co-founded with my boyfriend <a href="http://liamscott.me">Liam Scott</a>. Together we make the web shine.</p>
-					<p>For my life in bullet points, see my <a href="resume">résumé</a>. If you'd rather chat, send mail to <a href="mailto:kyle@kyledecker.me">kyle@kyledecker.me</a> or give me a call in Greensboro and we'll share conversation and coffee. Hope to hear from you soon.</p>
-				</div>
 			</div>
 		</main>
 	</div>

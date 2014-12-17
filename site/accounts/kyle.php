506b62fad1fefa91b57db1d8aa7d783bf8e82b16
Kyle Decker <kylebdecker@gmail.com>
2014-12-17 15:27:32 +0100

    Responsive & content tweaks

--- site/accounts/kyle.php
+++ site/accounts/kyle.php
@@ -6,10 +6,10 @@ password: >
   $2a$10$.ZeuDX1OsnUpcJAoSXCxi.3DIwr.uD9KEnC5P9sKDb9bP6.2bvp/W
 language: en
 role: admin
-token: 872ccc0446b40a6348c6585a941b53adcb26a2b1
+token: 969f9c6149dc6adf26300d5e63e360e948ed6d13
 history:
+  - projects/tunetwin
+  - home
   - projects/winter-mix
   - projects/catnap
-  - projects/tunetwin
   - projects
-  - resume

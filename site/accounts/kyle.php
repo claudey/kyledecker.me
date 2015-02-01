87658ab473e75fd9ac3b86f369baf5c98b5131a8
Kyle Decker <kylebdecker@gmail.com>
2015-02-01 13:41:22 -0500

    Copy tweaks

--- site/accounts/kyle.php
+++ site/accounts/kyle.php
@@ -6,10 +6,10 @@ password: >
   $2a$10$.ZeuDX1OsnUpcJAoSXCxi.3DIwr.uD9KEnC5P9sKDb9bP6.2bvp/W
 language: en
 role: admin
-token: a9503e78a51d7e46b1b5a006b51618fddd03a24d
+token: 11ecfa5b9efb28203857fa325f3ce53acad019f1
 history:
+  - resume
   - projects/nintendo-pipeline
   - projects/catnap
   - projects/tunetwin
   - projects/winter-mix
-  - projects
